108510c8db2afbffad43aba8da3719db3c4c3795
firdavs9777 <fmutalipov97@gmail.com>
2025-07-28 16:01:02 +0900

    root state context created for the application

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, ReactNode } from "react";
+
+
+
+interface Task {
+  id: string;
+  text: string;
+}
+interface List {
+  id: string;
+  text: string;
+  tasks: Task[]
+}
+export interface AppState {
+  lists: List[]
+}
+
+const appData: AppState =  {
+  lists: [
+    {
+      id: "0",
+      text: 'To do',
+      tasks: [{id: 'c0', text: "Generate app scaffold"}]
+    },
+    {
+      id: "1",
+      text: 'In Progress',
+      tasks: [{id: 'c1', text: "Learn Typescript"}]
+    },
+    {
+      id: "2",
+      text: 'Done',
+      tasks: [{id: 'c2', text: "Begin to use static typing"}]
+    }
+  ]
+}
+
+export const AppStateContext = createContext< {state: AppState} | undefined>(undefined);
+export const AppStateProvider = ({ children }: React.PropsWithChildren) => {
+  return (
+    <AppStateContext.Provider value={{state: appData}}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
